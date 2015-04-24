46ce786338b46d115ab374c7e65db9a4baffe82b
Morgan <mlaupies@evenium.com>
2015-04-24 19:00:11 +0200

    initial setup

---
+++ make-webpack-config.js
@@ -0,0 +1,117 @@
+/*
+ * Webpack server configuration
+ *
+ * For dev, config is set up for serving the webpack-dev-server, which will watch for changes and recompile as required if
+ * the subfolder /webpack-dev-server/ is visited. Visiting the root will not automatically reload.
+ *
+ * For production, config is set up for serving the distribution version. It will be compiled to dist/ by default
+ */
+'use strict';
+
+var webpack = require("webpack");
+var path = require("path");
+
+/**
+ * http://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
+ */
+function escapeRegExp(str) {
+	return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+function buildConfig(options) {
+	var dev = options.dev;
+
+	// Include js-common as source path to be process by babel loader (jsx and ES6 support)
+	var currentSrc = escapeRegExp(path.join(__dirname, 'src'));
+
+	// Entry
+	var entry;
+	if (!dev) {
+		entry = './src/tooltip.jsx';
+	} else {
+		entry = ['webpack/hot/only-dev-server', './src/tooltip.jsx'];
+	}
+
+	// Output
+	var output = {
+		publicPath: '/assets/',
+		filename: 'tooltip.bundle.js'
+	};
+	if (!dev) {
+		output.path = 'dist/assets/';
+	}
+
+	// Plugins
+	var plugins;
+	if (!dev) {
+		plugins = [
+			new webpack.optimize.DedupePlugin(),
+			new webpack.optimize.UglifyJsPlugin(),
+			new webpack.optimize.OccurenceOrderPlugin(),
+			new webpack.optimize.AggressiveMergingPlugin()
+		];
+	} else {
+		plugins = [
+			new webpack.HotModuleReplacementPlugin(),
+			new webpack.NoErrorsPlugin()
+		];
+	}
+
+	// js/jsx loader
+	var jsLoader = {
+		test: /\.(js|jsx)$/,
+		include: currentSrc
+	};
+	if (!dev) {
+		jsLoader.loader = 'babel-loader';
+	} else {
+		jsLoader.loaders = ['react-hot-loader', 'babel-loader'];
+	}
+
+	var config = {
+		cache: dev,
+		debug: dev,
+
+		entry: entry,
+		output: output,
+
+		stats: {
+			colors: true,
+			reasons: dev
+		},
+
+		resolve: {
+			extensions: ['', '.js', '.jsx']
+		},
+
+		module: {
+			preLoaders: [{
+				test: /\.(js|jsx)$/,
+				include: currentSrc,
+				loader: 'jsxhint'
+			}],
+			loaders: [
+				{ test: /\.css$/, loader: 'style-loader!css-loader' },
+				{ test: /\.(jpe?g|png|gif)$/, loader: 'url-loader?limit=10000' },
+				jsLoader
+			]
+		},
+
+		plugins: plugins,
+
+		headers: {
+			"Access-Control-Allow-Origin": "*"
+		}
+	};
+
+	// Add sourceMap for dev
+	if (dev) {
+		config.devtool = 'source-map';
+	}
+
+	return config;
+}
+
+module.exports = function(options) {
+	return buildConfig(options);
+};
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = require("./make-webpack-config")({
+	dev: true
+});
